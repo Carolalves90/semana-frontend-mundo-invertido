d3b043fba5ec42252eb7b06f62e92881bb91b328
Carol Alves <prof.carolalves90@gmail.com>
2022-08-24 22:57:50 -0300

    :sparkles: adicionando js

---
+++ assets/js/firebase/app.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.9.3/firebase-app.js";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBgi6xH6MXVTB2O74Etkv2wDPJemDmobEE",
+  authDomain: "projeto-teste-2885c.firebaseapp.com",
+  projectId: "projeto-teste-2885c",
+  storageBucket: "projeto-teste-2885c.appspot.com",
+  messagingSenderId: "671211192339",
+  appId: "1:671211192339:web:82693776faeb2141bdbfe6",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app
